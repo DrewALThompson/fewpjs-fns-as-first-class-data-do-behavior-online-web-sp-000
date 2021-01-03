fcc97592de1c610b7f562c619a3082dc1343c6b5
Drew Thompson <drewallynthompson@gmail.com>
2021-01-03 06:43:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,5 +9,7 @@ function handleClick(e) {
 
 /* End Given Code, don't edit above here...*/
 
-/* Write your implementation of greet() */
+function greet(string){
+  if string.to
+};
 /* Write your implementation of displayMessage() */
